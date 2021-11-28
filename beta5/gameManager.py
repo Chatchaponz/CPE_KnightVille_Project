81ea56c81935059aaa44c5fa5de1349628079eb4
Chatchapon <leawgamer1@hotmail.com>
2021-11-28 08:56:31 +0700

    Update gameManager.py

--- beta5/gameManager.py
+++ beta5/gameManager.py
@@ -294,7 +294,7 @@ class GameManager(GameScreen):
             # Set other player as host
             if self.playersData != [] and hostAddr != None:
                 for player in self.playersData:
-                    if player != self.player and thisAddr == player.address:
+                    if player != self.player and hostAddr == player.address:
                         player.host = True
                     else:
                         player.host = False
